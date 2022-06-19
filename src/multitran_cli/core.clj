dc0a46b9875e100deedaa58035e2117465493ad7
Lukin Egor <lukin.net@gmail.com>
2022-06-19 13:51:27 +0400

    added fetch function

--- src/multitran_cli/core.clj
+++ src/multitran_cli/core.clj
@@ -1,14 +1,51 @@
-(ns multitran-cli.core)
+(ns multitran-cli.core
+  (:require [clojure.string :as str]
+            [clj-http.client :as client]))
 
-(def url "https://context.reverso.net/translation/english-russian/hello+world")
+(def host "https://context.reverso.net/")
 
 (def SOURCE_COLUMN_PATTERN #"<div class=\"src ltr\">\s*<span class=\"text\">\s*(.*)\s*<\/span>\s*<\/div>")
 (def TARGET_COLUMN_PATTERN #"<div class=\"trg ltr\">\s*<span class=\"icon jump-right\"><\/span>\s*<span class=\"text\" lang=\'.{2}\'>\s*(.*)\s*<\/span>\s*<\/div>")
+(def REMOVE_HTML_TAGS #"<\/?[^>]*>")
+
+(def user-agents ["Mozilla/5.0"])
+(def accept-header "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+
+(defn remove-tags [content]
+  (str/replace content REMOVE_HTML_TAGS ""))
+
+(defn transform [content]
+ (-> content
+     (nth 1)
+     (remove-tags)))
 
 (defn extract [content]
   (let [source-column (re-seq SOURCE_COLUMN_PATTERN content)
         target-column (re-seq TARGET_COLUMN_PATTERN content)]
     (map
      vector
-     (map #(nth % 1) source-column)
-     (map #(nth % 1) target-column))))
+     (map #(transform %) source-column)
+     (map #(transform %) target-column))))
+
+(defn format-phraze [phraze]
+  (str/replace phraze #"\s+" "+"))
+
+(defn compose-url
+  ([phraze] (compose-url phraze "english" "russian"))
+  ([phraze source-lang target-lang]
+   (str host "translation/" source-lang "-" target-lang "/" (format-phraze phraze))))
+
+(defn fetch [phraze]
+  (-> phraze
+      (compose-url)
+      (client/get
+       {:headers
+        {"User-Agent" (rand-nth user-agents),
+         "Accept" accept-header}})))
+
+(defn fetch-and-extract
+  [phraze]
+  (-> phraze
+      (fetch)
+      (:body)
+      (extract)))
--- test/multitran_cli/core_test.clj
+++ test/multitran_cli/core_test.clj
@@ -4,25 +4,9 @@
 
 (def fixture-path "test/fixtures/content.html")
 
-(def html (slurp fixture-path))
-
-(get-in [1] [0])
-
-a
-(first a)
-[(first a) (second 2)]
-
-
-(nth a 0)
-(get a 0)
-items
 (deftest test-html-extracting
-  (def items (extract html))
-  (get-in items [0 0])
-  a
-  ;; (def content (slurp fixture-path))
-
-  (nth items 0)
+  (let [html (slurp fixture-path)
+        items (extract html)]
   (testing ""
-    (is (= (get-in items [0 0]) "test")))
-  )
+    (is (= "This will write \"hello world\" on the display." (nth (nth items 0) 0)))
+    (is (= "В результате на индикаторе появится \"hello world\"." (nth (nth items 0) 1))))))
